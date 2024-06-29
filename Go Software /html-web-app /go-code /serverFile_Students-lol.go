6aec295f470cbb1a017b4b476d5432fce23ebcf4
kyle2501@gmail.com <kyle2501@gmail.com>
2024-06-29 09:54:27 -0700

    update serverFile_Students-lol.go

--- Go Software /html-web-app /go-code /serverFile_Students-lol.go
+++ Go Software /html-web-app /go-code /serverFile_Students-lol.go
@@ -42,3 +42,82 @@ func app_welcome_center_page() {
     
 }
 
+
+
+// . appHandler
+func appHandler(w http.ResponseWriter, r *http.Request) {
+
+    if r.URL.Path != "/app" {
+    	http.NotFound(w, r)
+    	return
+    }
+    
+// ,
+
+  pageTitle := "www.Students.lol/ - Website App"
+  pagePath := r.URL.Path
+  
+  pageType := ".."
+  
+  
+  pageData := htmlPageData {
+      pageTitle: pageTitle,
+      pagePath: pagePath,
+      
+      pageList: []pageNav {
+          { pageTitle: "one", pageLink: "one"},
+          { pageTitle: "two", pageLink: "two"},
+          { pageTitle: "three", pageLink: "three"},
+      },
+  	
+  }  //. .  pageData
+  
+  
+  
+
+ 
+  
+  
+  
+  pageFilePath := template.Must(template.ParseFiles("layout_main_page.html"))
+  pageFilePath.Execute(w, pageData)
+  
+}  //  .  appHandler
+
+
+
+
+//  .  html url routes 
+//  .  as well as terminal cli logs
+
+func main() {
+
+  appName := "www.Students.lol/ - Website App"
+
+
+
+
+
+
+
+// -- -
+  port := os.Getenv("PORT")
+  if port == "" {
+    port = "8080"
+    log.Printf("Loading _webapp with default port")
+  }
+  
+  
+  log.Printf("_webapp is active and Listening on port %s", port)
+
+  log.Printf("// -- - %s", appName)
+  log.Printf("_webapp now loaded and running at http://localhost:%s", port)
+
+// -- - 
+  if err := http.ListenAndServe(":"+port, nil); err != nil {
+    log.Fatal("Error Starting the HTTP Server :", err)
+    return
+  }
+
+
+}
