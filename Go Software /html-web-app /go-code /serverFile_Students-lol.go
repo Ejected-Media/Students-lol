d00df7deb0df611f7d31cbe73e7dc0fe867d580f
kyle2501@gmail.com <kyle2501@gmail.com>
2024-06-29 09:51:13 -0700

    update serverFile_Students-lol.go

--- Go Software /html-web-app /go-code /serverFile_Students-lol.go
+++ Go Software /html-web-app /go-code /serverFile_Students-lol.go
@@ -3,14 +3,42 @@
 package main
 
 
-
+// .
+  
 import (
+  "os"
+  "log"
+		
+  "text/template"
+  "net/http"
+		
+  "cloud.google.com/go"
     
     
 )
+
+
+
+type htmlPageData struct {
+    pageTitle string
+    pagePath string
+    pageList []pageNav
+    
+}
+
+
+type pageNav struct {
+    pageTitle string
+    pageLink string
+}
+
+
+
+
 
 
 func app_welcome_center_page() {
     
     
 }
+
